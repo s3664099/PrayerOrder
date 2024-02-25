dcf87ca17c179a7a4d3c6de4872875e8d4f66c79
Dave Sarkies <s3664099@student.rmit.edu.au>
2024-02-25 11:52:42 +1100

    Started building user authentication front end

---
+++ js/user.js
@@ -0,0 +1,17 @@
+/*
+File: PrayerOrder user JS Scripts Page
+Author: David Sarkies 
+Initial: 25 February 2024
+Update: 25 February 2024
+Version: 0.0
+*/
+
+function validateLogin() {
+
+	email = document.getElementById("email")
+
+}
+
+/*
+25 February 2024 - Created file
+*/
--- signin.php
+++ signin.php
@@ -2,8 +2,8 @@
 File: PrayerOrder Sign In Page
 Author: David Sarkies 
 Initial: 10 November 2023
-Update: 12 November 2023
-Version: 0.2
+Update: 25 February 2024
+Version: 0.3
 
 - Validate - Needs to be either email or phone - validates to make sure that either are used
 
@@ -25,20 +25,20 @@ Version: 0.2
 	      src="./Images/title.png">
       </div>
       <div class="main-section">
+         <div id="authenticationFailure"></div>
          <form method="post" action="<?php echo htmlspecialchars('authenticate.php');?>" id="sign_in">
             <h3 style="text-align: center;">Email</h3>
-            <input type="text" name="email" id="email" class="centre"/>
-            <h3 style="text-align: center;">Phone</h3>
-            <input type="text" name="phone" id="phone" class="centre"/>            
+            <input type="text" name="email" id="email" class="centre"/>          
             <h3 style="text-align: center;">Password</h3>
             <input type="password" name="password" id="password" class="centre">
             <div class="button-div">
-               <button class="left-button">Sign In</button>
+               <button class="left-button" onclick="validateLogin();">Sign In</button>
                <button class="right-button" onclick="change_action('signup.php','sign_in')">Sign Up</button>
             </div>
          </form> 
       </div>
       <script type="text/javascript" src="/js/standard.js"></script>
+      <script type="text/javascript" src="/js/user.js"></script>
 	 <!-- Username/Password in form -->
          <!-- sign-in/sign-up button -->
 	 <!-- sign-in authenticates and saves session --> 
@@ -52,4 +52,5 @@ Version: 0.2
    10 November 2023 - Created Page
    11 November 2023 - Configured title and added sign in form
    12 November 2023 - Added styling
+   25 February 2024 - Started added sign-in authentication code
 -->
