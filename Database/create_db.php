d2bd237ee917422a112ef07ee07527510ece9359
Dave Sarkies <s3664099@student.rmit.edu.au>
2024-06-27 19:29:34 +1000

    Created database with user table

--- Database/create_db.php
+++ Database/create_db.php
@@ -1,5 +1,17 @@
 <?php
 
+//execute query
+function execute_query($conn,$sql) {
+
+	if ($conn->query($sql) === TRUE) {
+	  echo "Database created successfully";
+	} else {
+  	echo "Error creating database: " . $conn->error;
+	}
+
+}
+
+//Loads authentication for json file
 $json = file_get_contents('db_login.json');
 $json_data = json_decode($json,true);
  echo $json_data['pw'];
@@ -7,14 +19,22 @@ $json_data = json_decode($json,true);
 $servername = $json_data['name'];
 $username = $json_data['user'];
 $password = $json_data['pw'];
+$dbname = $json_data['dbname'];
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} else {
-	echo "Success\n";
-}
+} 
+
+$sql = "CREATE TABLE user(name VARCHAR(50),email VARCHAR(200),phone VARCHAR(10),
+													regdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,password VARCHAR(256))";
+execute_query($conn,$sql);
+// Create database
+//$sql = "CREATE DATABASE prayerorder";
+
+$conn->close();
 
 ?>
