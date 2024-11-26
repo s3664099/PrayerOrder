47da26fa62658debfd086680d9e31afa644c7967
Dave Sarkies <s3664099@student.rmit.edu.au>
2024-11-26 19:30:03 +1100

    created border for prayers

--- includes/prayers.php
+++ includes/prayers.php
@@ -3,8 +3,8 @@
 File: PrayerOrder prayers page
 Author: David Sarkies 
 #Initial: 24 November 2024
-#Update: 24 November 2024
-#Version: 0.0
+#Update: 26 November 2024
+#Version: 0.1
 */
 
 include 'pray.php';
@@ -19,7 +19,7 @@ foreach ($result as $x) {
 	#$date = date_create_from_format("dMYGis',$x['postdate']); #strtotime($x['postdate']);
 	#$time = date_diff($date,date());
 
-	echo "<pre><h4>";
+	echo "<pre><h4 class='user-header'>";
 	
 	if (strlen($x['image'])>0) {
 		echo "<img id='avatar' alt='user_image' width='15' src='./Images/Avatar/".$x['image']."'>";
@@ -28,12 +28,14 @@ foreach ($result as $x) {
 	}
 
 	echo $x['name']."</h4>";
-	echo $diff->format('%y years, %m months, %d days, %h hours, %i minutes, %s seconds')."</br>".$x['prayerkey']."</br>";
-	echo $x['image']."</br></br>";
+	echo "<div class='user-header'>".$diff->format('%y years,%m months, %d days, %h hours, %i minutes, %s seconds')."</div>";
+	echo "<div class='user-header'>".$x['prayerkey']."</div>";
+	echo "</br>";
 	echo "</pre>";
 }
 
 /*
 24 November 2024 - Created File
+26 November 2024 - Moved the user header to the left
 */
 ?>
