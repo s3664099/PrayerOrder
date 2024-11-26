d131e4a0e2b6f205b0c693b52c550bf1d1aeeb01
Dave Sarkies <s3664099@student.rmit.edu.au>
2024-11-26 20:12:59 +1100

    Added code to determine time since post and updated styling

--- includes/prayers.php
+++ includes/prayers.php
@@ -14,10 +14,6 @@ $result = getPrayers($_SESSION['user']);
 foreach ($result as $x) {
 
 	$postDate = new DateTime($x['postdate']);
-	$currDate = new DateTime();
-	$diff = $currDate->diff($postDate);
-	#$date = date_create_from_format("dMYGis',$x['postdate']); #strtotime($x['postdate']);
-	#$time = date_diff($date,date());
 
 	echo "<pre><h4 class='user-header'>";
 	
@@ -28,14 +24,40 @@ foreach ($result as $x) {
 	}
 
 	echo $x['name']."</h4>";
-	echo "<div class='user-header'>".$diff->format('%y years,%m months, %d days, %h hours, %i minutes, %s seconds')."</div>";
+	echo "<div class='user-header'>".datediff($postDate)."</div>";
 	echo "<div class='user-header'>".$x['prayerkey']."</div>";
 	echo "</br>";
 	echo "</pre>";
 }
 
+function datediff($pastdate) {
+
+	$currDate = new DateTime();
+	$date = "";
+
+	// Calculate the difference
+	$date_diff = $currDate->diff($pastdate);
+
+	// Determine the most significant unit
+	if ($date_diff->y > 0) {
+    	$date = $date_diff->y . " year" . ($date_diff->y > 1 ? "s" : "") . " ago";
+	} elseif ($date_diff->m > 0) {
+    	$date = $date_diff->m . " month" . ($date_diff->m > 1 ? "s" : "") . " ago";
+	} elseif ($date_diff->d > 0) {
+    	$date = $date_diff->d . " day" . ($date_diff->d > 1 ? "s" : "") . " ago";
+	} elseif ($date_diff->h > 0) {
+    	$date = $date_diff->h . " hour" . ($date_diff->h > 1 ? "s" : "") . " ago";
+	} elseif ($date_diff->i > 0) {
+    	$date = $date_diff->i . " minute" . ($date_diff->i > 1 ? "s" : "") . " ago";
+	} else {
+    	$date = "Just now";
+	}
+
+	return $date;
+}
+
 /*
 24 November 2024 - Created File
-26 November 2024 - Moved the user header to the left
+26 November 2024 - Moved the user header to the left. Added function to determine when prayer was posted.
 */
 ?>
