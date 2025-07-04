9da331b30bef591ae5eb165a47cfdb6a8384a46b
Dave Sarkies <s3664099@student.rmit.edu.au>
2025-07-04 23:49:50 +1000

    Added initial scripts to update prayer db

--- Database/db_update_prayer.php
+++ Database/db_update_prayer.php
@@ -7,7 +7,38 @@ Update: 20 June 2025
 Version: 1.1
 */
 
+include 'db_handler.php';
 
+$failed = False;
+$db = new db_handler('db_root.json');
+$conn = $db->get_connection();
+
+$users = swap_prayer_data($conn,'prayerorder');
+
+function execute_query($conn,$sql) {
+
+	if ($conn->query($sql) === TRUE) {
+		echo "Query Executed successfully\n";
+	} else {
+  		echo "Executing query: " . $conn->error."\n";
+	}
+}
+
+function retrieve_data($conn,$sql) {
+
+	$stmt = $conn->prepare($sql);
+	$stmt->execute();
+	return $stmt->get_result();
+}
+
+function swap_prayer_data($conn) {
+	
+}
+
+/*
+po_user
+prayerorder
+*/
 
 
 /* Created File
