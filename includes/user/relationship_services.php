2322ae58c5f979fee34b00898cec313297bf81c6
Dave Sarkies <s3664099@student.rmit.edu.au>
2025-11-22 14:42:37 +1100

    User search now works

--- includes/user/relationship_services.php
+++ includes/user/relationship_services.php
@@ -3,8 +3,8 @@
 File: PrayerOrder Relationship Service
 Author: David Sarkies 
 Initial: 18 November 2025
-Update: 18 November 2025
-Version: 1.0
+Update: 22 November 2025
+Version: 1.1
 */
 
 include '../database/db_prayer_ro.php';
@@ -13,28 +13,63 @@ class relationship_services {
 	
 	private static $db_prayer_ro;
 
+	private const REL_NONE = 0;
+	private const REL_FOLLOWED = 1;
+	private const REL_FRIENDS = 2;
+	private const REL_BLOCKED = 3;
+	private const REL_FOLLOWING = 4;
+	private const REL_BLOCKING = 5;
+
 	// Initialize DB objects once
     function __construct() {
         self::$db_prayer_ro = new db_prayer_ro();
     }
 
-    function get_relationship($current_user,$other_user) {
+    function get_relationship($current_user,$other_user,$user_no) {
 
-    	$relationship = get_relationship($_SESSION['user'],$x['id']);
+    	$relationship = self::get_relationship_type($_SESSION['user'],$other_user['id'],$user_no);
 
 		//This should be handled in relationship as well
 		if ($relationship != 3) {
-			$x['no'] = "user".$user_no;
-			$x['relationship'] = transcodeRelationship($relationship);
+			$other_user['no'] = "user".$user_no;
+			$other_user['relationship'] = transcodeRelationship($relationship);
 			$user_no++;
-			$users[] = $x;
 		}
+		return $other_user;
     }
 
+    function get_relationship_type($user,$otherUser) {
 
+		$relationship = self::REL_NONE;
+		$relResult = self::$db_prayer_ro->getRelationship($otherUser,$user);
+
+		if($relResult->num_rows>0) {
+			$relationship = $relResult->fetch_assoc()['followType'];
+		}
+
+		//Has user been blocked?
+		if ($relationship ==self::REL_BLOCKING) {
+			$relationship = self::REL_BLOCKED;
+		}
+
+		//No relationship found
+		if ($relationship == self::REL_NONE) {
+			$relResult = self::$db_prayer_ro->getRelationship($user,$otherUser);
+
+			if($relResult->num_rows>0) {
+				$relationship = $relResult->fetch_assoc()['followType'];
+
+				if ($relationship==self::REL_FOLLOWED) {
+					$relationship=self::REL_FOLLOWING;
+				}
+			}
+		}
+		return $relationship;
+	}
 }
 
 /*
 18 November 2025 - Created File
+22 November 2025 - Added relationship processing
 */
 ?>
--- includes/user/user_services.php
+++ includes/user/user_services.php
@@ -3,8 +3,8 @@
 File: PrayerOrder User Service
 Author: David Sarkies 
 Initial: 18 November 2025
-Update: 18 November 2025
-Version: 1.0
+Update: 22 November 2025
+Version: 1.1
 */
 
 include '../database/db_user_ro.php';
@@ -23,50 +23,20 @@ class user_services {
     	$users = [];
     	$relationship_service = new relationship_services();
 
-		$allUsers = self::$db_user->getUsers($_GET['users'],$_SESSION['user']);
+		$allUsers = self::$db_user_ro->getUsers($_GET['users'],$_SESSION['user']);
 		$user_no = 0;
 	
-		while ($x = $allUsers->fetch_assoc()) {
-			$users[] = $relationship_service->get_relationship($_SESSION['user'],$x);
+		while ($other_user = $allUsers->fetch_assoc()) {
+			$users[] = $relationship_service->get_relationship($_SESSION['user'],$other_user,$user_no);
+			$user_no ++;
 		}
 
 		return $users;
     }
-
-    //Move to relationship services
-    function getRelationship($user,$otherUser) {
-
-	$db_prayer = new db_prayer_ro();
-	$relationship = 0;
-	$relResult = $db_prayer->getRelationship($otherUser,$user);
-
-	if($relResult->num_rows>0) {
-		$relationship = $relResult->fetch_assoc()['followType'];
-	}
-
-	//Has user been blocked?
-	if ($relationship ==5) {
-		$relationship = 3;
-	}
-
-	//No relationship found
-	if ($relationship == 0) {
-		$relResult = $db_prayer->getRelationship($user,$otherUser);
-
-		if($relResult->num_rows>0) {
-			$relationship = $relResult->fetch_assoc()['followType'];
-
-			if ($relationship==1) {
-				$relationship=4;
-			}
-		}
-	}
-	return $relationship;
-}
-
 }
 
 /*
 18 November 2025 - Created File
+22 November 2025 - Cleaned up file
 */
 ?>
