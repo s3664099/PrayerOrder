c755c0a6caec7f8aa2e0cdb19ceec96988a5d1e2
Dave Sarkies <s3664099@student.rmit.edu.au>
2026-09-13 12:35:11 +1000

    Created Added invite items

--- includes/templates/invite_item.php
+++ includes/templates/invite_item.php
@@ -3,26 +3,22 @@
 File: PrayerOrder invite item template
 Author: David Sarkies 
 #Initial: 12 September 2026
-#Update: 12 September 2026
-#Version: 1.0
+#Update: 13 September 2026
+#Version: 1.1
 */
 ?>
 
-
-
+<h4 id='<?= htmlspecialchars($invite['groupKey'], ENT_QUOTES, 'UTF-8') ?>' class='accept_invite'>
+	<img alt='Accept Invite' width='15' src='./Images/icon/accept.png/' onclick='acceptInvite(this)' 
+		 class='accept_invite' title='Accept'>
+	<img alt='Reject Invite' width='15' src='./Images/icon/reject.png/' onclick='rejectInvite(this)' 
+		  class='accept_invite' title='Reject'>
+	Invite: <?= htmlspecialchars($invite['groupName'], ENT_QUOTES, 'UTF-8') ?>
+</h4>
 
 
 <?php
 /* 12 September 2026 - Created File
-
-
-	echo "<h4 id='".$x['groupKey']."' class='accept_invite'>";
-	echo "<img alt='Accept Invite' width='15' src='./Images/icon/accept.png/' onclick='acceptInvite(this)' 
-		  class='accept_invite' title='Accept'>";
-		echo "<img alt='Reject Invite' width='15' src='./Images/icon/reject.png/' onclick='rejectInvite(this)' 
-		  class='accept_invite' title='Reject'>";
-	echo "Invite: ".$x['groupName']."</h4>";
-
-
+ * 13 September 2026 - Added invites
 */
 ?>
