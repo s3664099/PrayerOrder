8c0fbfc61f590172aef1d7ae008b6f9981ee8775
Dave Sarkies <s3664099@student.rmit.edu.au>
2026-09-17 22:32:42 +1000

    Updated group styling

--- includes/templates/group_item.php
+++ includes/templates/group_item.php
@@ -12,7 +12,7 @@ Author: David Sarkies
 	<?php if ($group['memberType']=='m') {
 		?><span class='pl-15p'><?php
 	} else if ($group['memberType']=='c' || $group['memberType']=='a') {
-		?><span><img src='./Images/icon/admin.png' width='20' class='pr-2p'><?php
+		?><span><img src='./Images/icon/admin.png' width='20' class='pl-2p pr-2p'><?php
 	}?>
 		<button class='groupSelect' onclick='selectGroup(this)' id='<?= htmlspecialchars($group["groupKey"],ENT_QUOTES,"UTF-8")?>'>
 			<?=htmlspecialchars($group['groupName'],ENT_QUOTES,"UTF-8")?>
