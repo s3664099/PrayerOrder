cd34b7e82bc32a56633690ab98c0c70dbf64691f
Dave Sarkies <s3664099@student.rmit.edu.au>
2026-09-19 22:01:38 +1000

    Started building group member page

--- includes/group/group_functions.php
+++ includes/group/group_functions.php
@@ -11,28 +11,7 @@ include 'includes/database/db_functions.php';
 $db = new db_functions();
  
 //member type - m - member, p - pending, b - blocked, c - creator, a - admin
-function display_groups() {
 
-	foreach (get_groups($_SESSION['user']) as $x) {
-
-        if ($x['memberType'] != 'p' && $x['memberType'] != 'b') {
-            echo "<div>";
-         	if ($x['memberType']=='m') {
-            	echo "<span class='pl-15p'>";
-            } else if ($x['memberType'] == 'c' || $x['memberType']=='a') {
-                echo "<span><img src='./Images/icon/admin.png' width='20' class='pr-2p'>";
-            }
-
-            echo "<button class='groupSelect' onclick='selectGroup(this)' id='".$x['groupKey']."'>".$x['groupName'];
-            echo "</button></span></div>";
-        }
-    }
-}
-
-function get_groups($user) {
-    $db = new db_functions();
-	return $db->getGroups($user);
-}
 
 function set_group_name() {
     $db = new db_functions();
@@ -41,36 +20,7 @@ function set_group_name() {
 
 //Add function to display membership type if user is an admin
 
-function getMembers() {
 
-    $db = new db_functions();
-    $result = $db->getMembers($_SESSION['groupId']);
-    
-    echo ("<div class='group-prayer-box'>");
-    foreach ($result as $x){
-        displayMembers($x);
-    }
-    echo ("</div>");
-}
-
-function displayMembers($member) {
-    echo("<h3 class='prayer-h3'>".$member['name']." ");
-    $memberType = getMemberType($member['memberType']);
-    echo($memberType);            
-    echo("</h3>");
-}
-
-function getMemberType($member) {
-
-    $memberType = "";
-    if ($member == "a") {
-        $memberType = "- Admin";
-    } else if ($member == "c") {
-        $memberType = "- Creator";
-    }
-    return $memberType;
-
-}
 
 function getPrayerBox() {
     $db = new db_functions();
--- includes/group/group_members.php
+++ includes/group/group_members.php
@@ -7,6 +7,45 @@ Author: David Sarkies
 #Version: 1.0
 */
 
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/group/group_services.php';
+
+$group_service = new group_services();
+$result = $group_service->get_members($$_SESSION['group']['groupKey'])
+
+
+/*
+function getMembers() {
+
+    $db = new db_functions();
+    $result = $db->getMembers($_SESSION['groupId']);
+    
+    echo ("<div class='group-prayer-box'>");
+    foreach ($result as $x){
+        displayMembers($x);
+    }
+    echo ("</div>");
+}
+
+function displayMembers($member) {
+    echo("<h3 class='prayer-h3'>".$member['name']." ");
+    $memberType = getMemberType($member['memberType']);
+    echo($memberType);            
+    echo("</h3>");
+}
+
+function getMemberType($member) {
+
+    $memberType = "";
+    if ($member == "a") {
+        $memberType = "- Admin";
+    } else if ($member == "c") {
+        $memberType = "- Creator";
+    }
+    return $memberType;
+
+}
+*/
+
 
 /*
  * 19 September 2026 - Created File
